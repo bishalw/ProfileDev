2473dbe6c6725fa28c7e4c338ce9b29681a579f7
Bishal Wagle <bishalw@gmail.com>
2020-02-09 20:02:13 -0800

    server up running and connected with database

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// can also just do connect(db)
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    //if something goes wrong
+    console.error(err.message);
+    //exit process with failure
+    process.exit(1);
+  }
+};
+//export
+
+module.exports = connectDB;
