9bea65f364a4d44843dc8535829e1359ffcb34ca
Bishal Wagle <bishalw@gmail.com>
2020-02-10 15:39:02 -0800

    more router setup and testing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-const email = req.body.email;
-const password = req.body.email;
+router.post(
+  '/',
+  [
+    check('name')
+      .not()
+      .isEmpty()
+      .withMessage('Name is needed'),
+    check('email')
+      .isEmail()
+      .withMessage('Username cannot be empty.'),
+    check(
+      'password',
+      'Please enter a password with 6 or more character'
+    ).isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(req.body);
 
-router.route('/').get((req, res) => {
-  console.log();
-  res.send('');
-});
-router.route('/add').add((req, res) => {});
+    // If there are errors!!  if(errors.isEmpty()) = if there is not (!errors). ! gives u errors
+    if (!errors.isEmpty()) {
+      console.log(errors.mapped());
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,12 @@ connectDB();
 //test out endpoint
 app.get('/', (req, res) => res.send('API Running'));
 
+//Define the routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
